7fb5b153d508b1b5ea95cde8cfc4f1d44ba6c0f4
samuel1012023 <125586124+samuel1012023@users.noreply.github.com>
2023-02-16 11:03:06 -0500

    Add files via upload

---
+++ ejemplo.py
@@ -0,0 +1,10 @@
+
+nombre=input("cuales es tu nombre")
+print("hola",nombre)
+edad=input("cual es tu edad")
+print(edad,"cual es tu numero de la suerte")
+quetegusta=((input,"que te gusta"))
+num1=float(("digita el primer numero"))
+nume2=float(("digita el segundo numero"))
+suma= num1 + nume2
+print(nombre,"el resultado de la suma es ..."suma)
